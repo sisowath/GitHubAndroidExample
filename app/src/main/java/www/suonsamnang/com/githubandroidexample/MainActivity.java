eaed7db92944e32a7b276b0a81c6aeb9d0c8d65c
Samnang <samnangsuon.ss@gmail.com>
2016-04-19 23:40:01 -0400

    Version du 19 avril 2016 (23h39) Par SUON Samnang
    + Modification du message affiché.

--- app/src/main/java/www/suonsamnang/com/githubandroidexample/MainActivity.java
+++ app/src/main/java/www/suonsamnang/com/githubandroidexample/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        ((TextView)findViewById(R.id.lblHelloWorl)).setText("Dynamic Text !!!");
     }
 
     @Override
